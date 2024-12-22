f246218c0b47ab0d4851da279ab0864154c0c4f8
icon0907k <icon0907k@gmail.com>
2024-12-22 15:59:02 +0900

    프로젝트 파일 반영

---
+++ src/test/java/afterwork/millionaire/tradingBot/TradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package afterwork.millionaire.tradingBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
